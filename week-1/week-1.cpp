d47bf0517652d3e0a6cf34959ebf3b4f19f71c31
mohamed <m635379@gmail.com>
2023-04-03 23:05:30 +0200

    add assignments of week oen

---
+++ week-1/week-1.cpp
@@ -0,0 +1,91 @@
+// Assignment-1
+#include <iostream>
+
+int main_two();
+int main_three();
+int main_four();
+
+int main()
+{
+    std::cout << "Line One\n";
+    std::cout << "Line Two\n";
+    std::cout << "Line Three\n";
+
+    std::cout << "=======================\n";
+
+    main_two();
+
+    std::cout << "=======================\n";
+
+    main_three();
+
+    std::cout << "=======================\n";
+
+    main_four();
+    return 0;
+}
+
+// Output
+// ---Line One
+// ---Line Two
+// ---Line Three
+
+// Assignment-2
+
+/*
+  iostream Header File Content
+  -- cout
+  -- cin
+  -- cerr
+  -- clog
+  -- wcout 
+  -- wcin
+  -- wcerr
+  -- wclog
+*/
+
+// Assignment-3
+
+ main_two() {
+    std::cout << "Iam Learning Programming" << "\n";
+    std::cout << "With" << "\n";
+    std::cout << "Elzero" << "\n";
+    std::cout << "Web" << "\n";
+    std::cout << "School" << "\n";
+    std::cout << "<3" << "\n";
+    return 0;
+}
+
+// Iam Learning Programming
+// With
+// Elzero
+// Web
+// School
+// <3
+
+// Assignment-4
+
+ main_three() {
+    std::cout << "|=======================|" << "\n";
+    std::cout << "|== Elzero Web School ==|" << "\n";
+    std::cout << "|==        C++        ==|" << "\n";
+    std::cout << "|=======================|" << "\n";
+    return 0;
+}
+
+// Assignment-5
+
+// This Is My First File
+// In Learning Programming
+// Iam Happy <3
+// Iam Dragon
+
+#include <iostream> /* Preprocessor Directive */
+
+ main_four()
+{
+  std::cout /*Character Output*/ << "Line One\n";
+  std::cout << "Line Two\n"; /* cout Function */
+  std::cout << "Line Three\n";
+  return 0;
+} /* End Main Function*/
