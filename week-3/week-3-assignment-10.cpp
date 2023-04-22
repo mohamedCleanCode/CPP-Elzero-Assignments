0bdbb1b34c2c448cb1981b5f9b80e21b57811f6d
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2023-04-22 23:13:16 +0200

    end week-3

---
+++ week-3/week-3-assignment-10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Add Type Alias Here
+    using lli = long long int;
+    typedef long long int lli;
+
+    // Do Not Edit
+    lli num = 150050005; // This Is Long Long Int
+    cout << num << "\n"; // 150050005
+    return 0;
+}
---
+++ week-3/week-3-assignment-9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Edit Anything Except Values
+    unsigned int a = 100;
+    int b = -100;
+    short c = 100;
+    short d = 500.55;
+
+    // Do Not Edit
+    cout << a << "\n"; // 100
+    cout << b << "\n"; // -100
+    cout << c << "\n"; // 100
+    cout << d << "\n"; // 500.55
+    return 0;
+}
